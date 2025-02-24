c6d810b8445fb7e420e2d957455b849168b018b8
jpbmoura <jopmoura49@gmail.com>
2025-02-24 10:42:07 -0300

    change hero height

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 
 export default function Hero() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-52">
+    <section className="w-full h-vh85 flex items-center justify-center bg-center">
       <div className="container px-4 md:px-6 m-auto">
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="space-y-2">
